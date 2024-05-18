72a25bb108a5d7ddc2ba06f36af95eec2acf18ab
Payin14 <payin630@gmail.com>
2024-05-18 17:36:57 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,53 +1,22 @@
 #!/usr/bin/python3
-def convert_roman(ch):
-    ret = -1
-    if ch == 'I':
-        ret = 1
-    elif ch == 'V':
-        ret = 5
-    elif ch == 'X':
-        ret = 10
-    elif ch == 'L':
-        ret = 50
-    elif ch == 'C':
-        ret = 100
-    elif ch == 'D':
-        ret = 500
-    elif ch == 'M':
-        ret = 1000
-    return ret
-
-
 def roman_to_int(roman_string):
-    cur_max = -1
-    cur = conv = 0
-    holder = []
-
-    if roman_string is None or type(roman_string) is not str:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    for c in roman_string:
-        cur = convert_roman(c)
-        if cur == -1:
-            return 0
-        if len(holder) == 0:
-            if cur == cur_max or cur_max == -1:
-                cur_max = cur
-                conv += cur
-            elif cur < cur_max:
-                holder.append(cur)
-            elif cur > cur_max:
-                cur_max = cur
-                cur -= conv
-                conv = cur
-        else:
-            if cur > holder[-1]:
-                cur_max = cur
-                cur -= sum(holder)
-                conv += cur
-                holder.clear()
-            else:
-                holder.append(cur)
 
-    if len(holder) != 0:
-        conv += sum(holder)
-    return conv
+    roman_to_int_map = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_to_int_map.get(char, 0)
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    
+    return total
