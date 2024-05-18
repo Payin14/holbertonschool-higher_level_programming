69c4b060feae439f005ad0d05201a1c496044812
Payin14 <payin630@gmail.com>
2024-05-18 18:12:27 +0000

    a

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class with initialize size.
+Defaults size to 0. Raise error on invalid size inputs.
+Method area returns size of area of the square.
+"""
 
 
 class Square:
