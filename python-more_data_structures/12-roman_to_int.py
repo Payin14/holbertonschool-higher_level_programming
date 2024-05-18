1de3f969b57d138e78a54379ed23d8cbf039db9a
Payin14 <payin630@gmail.com>
2024-05-18 17:39:16 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
@@ -10,11 +11,14 @@ def roman_to_int(roman_string):
     
     total = 0
     prev_value = 0
+
     for char in reversed(roman_string):
         value = roman_to_int_map.get(char, 0)
+
         if value < prev_value:
             total -= value
         else:
             total += value
-        prev_value = value    
+        prev_value = value
+
     return total
