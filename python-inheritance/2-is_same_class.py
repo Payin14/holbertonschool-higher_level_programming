9615aa0d6b446f0ec99972cda44fec3f42b76a27
Payin14 <payin630@gmail.com>
2024-07-04 20:04:28 +0000

    changes

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-""" Python - Inheritance, task 2 """
+"""2-is_same_class.py
+"""
 
 
 def is_same_class(obj, a_class):
-    """Tests if an object is exactly an instance of a specific class.
-
-    Args:
-        obj (any): object of any type
-        a_class (class): class to test against
-
-    Returns:
-        True if obj is an instance of a_class, False otherwise.
-
-    """
-    return (type(obj) == a_class)
+    """ Compare is obj is exactly an instance of a_class """
+    return type(obj) == a_class
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-""" Python - Inheritance, task 4 """
+"""4-inherits_from.py
+"""
 
 
 def inherits_from(obj, a_class):
-    """Tests if an object is an instance of a class inherited from
-    the specified class, directly or indirectly.
-
-    Args:
-        obj (any): object of any type
-        a_class (class): class to test against
-
-    Returns:
-        True if obj is an instance of a subclass of a_class,
-            False otherwise.
-
-    """
-    if type(obj) == a_class:
-        return (False)
-    return (issubclass(type(obj), a_class))
+    """ Check for direct or indirect inheritance """
+    if (type(obj) != a_class):
+        return isinstance(obj, a_class)
+    return False
