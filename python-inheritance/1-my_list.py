9bdabe2b6d53bc1b66f36f2dc4b95be66c09afe8
Payin14 <payin630@gmail.com>
2024-07-04 20:02:43 +0000

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-""" Python - Inheritance, task 1 """
+'''1-my_list.py
+'''
 
 
 class MyList(list):
-    """Custom list type intended to only contain integers.
-
-    """
+    ''' This class inherits from list() '''
 
     def print_sorted(self):
-        """Prints MyList lists in ascending order by value.
-
-        """
-        sorted_list = self[:]
-        sorted_list.sort()
-        print(sorted_list)
+        """ prints the sorted list """
+        print(sorted(self))
