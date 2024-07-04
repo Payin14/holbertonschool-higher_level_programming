19ec361c975cf287b31da3e03ba06d30162a1cd8
Payin14 <payin630@gmail.com>
2024-07-04 20:20:00 +0000

    4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""4-inherits_from.py
-"""
+""" module returns true or false """
 
 
 def inherits_from(obj, a_class):
-    """ Check for direct or indirect inheritance """
-    if (type(obj) != a_class):
-        return isinstance(obj, a_class)
+    """ returns true if object is instance of a class """
+
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
     return False
